db43439177eee43f7c9cb36b93c0b6e955730d41
Uğur Berkant Ediz <108654354+UBerkant@users.noreply.github.com>
2023-12-28 23:10:28 +0300

    Update Odev.cs

--- Odev.cs
+++ Odev.cs
@@ -10,15 +10,15 @@ public class Odev : MonoBehaviour
     void bolenleriBul(int sayi1, int sayi2)
     {
         string listeString = "";
-        bool mod2Kontrol=false; // string içerisine atacak sayý olmamasý durumu için 
+        bool mod2Kontrol=false; // string icerisine atacak sayi olmamasý durumu icin 
         string mod2ListeString = "";
-        bool mod3Kontrol = false; // string içerisine atacak sayý olmamasý durumu için 
+        bool mod3Kontrol = false; // string icerisine atacak sayi olmamasý durumu icin 
         string mod3ListeString = "";
-        bool mod4Kontrol = false; // string içerisine atacak sayý olmamasý durumu için
+        bool mod4Kontrol = false; // string icerisine atacak sayi olmamasý durumu icin
         string mod4ListeString = "";
-        bool mod5Kontrol = false; // string içerisine atacak sayý olmamasý durumu için
+        bool mod5Kontrol = false; // string icerisine atacak sayi olmamasý durumu icin
         string mod5ListeString = "";
-        if(sayi1 <= sayi2) //Ýlk sayýnýn ikinci sayýdan küçük ve eþit olma durumu
+        if(sayi1 <= sayi2) //Ilk sayinin ikinci sayidan kucuk ve esit olma durumu
         {
             do
             {
@@ -44,9 +44,9 @@ public class Odev : MonoBehaviour
                     mod5Kontrol = true;
                 }
                 sayi1++;
-            }while (sayi1 <= sayi2); //eþitlenene kadar string içerisine atacak
+            }while (sayi1 <= sayi2); //esitlenene kadar string icerisine atacak
         }
-        else //ikinci sayýnýn birinci sayýdan küçük olma durumu
+        else //ikinci sayinin birinci sayidan kucuk olma durumu
         {
             do
             {
@@ -72,29 +72,29 @@ public class Odev : MonoBehaviour
                     mod5Kontrol = true;
                 }
                 sayi2++;
-            }while (sayi2 <= sayi1); //eþitlenene kadar string içerisine atacak
+            }while (sayi2 <= sayi1); //esitlenene kadar string icerisine atacak
         }
-        if (mod2Kontrol == false)// string içerisine attýðý sayý yok ise gireceði kontrol bloðu
+        if (mod2Kontrol == false)// string icerisine attigi sayi yok ise girecegi kontrol blogu
         {
-            mod2ListeString += "Ýkiye tam bölünen sayi bulunmamaktadýr.";
+            mod2ListeString += "Ikiye tam bolunen sayi bulunmamaktadir.";
         }
-        if (mod3Kontrol == false)// string içerisine attýðý sayý yok ise gireceði kontrol bloðu
+        if (mod3Kontrol == false)// string icerisine attigi sayi yok ise girecegi kontrol blogu
         {
-            mod3ListeString += "Üçe tam bölünen sayi bulunmamaktadýr.";
+            mod3ListeString += "Uce tam bolunen sayi bulunmamaktadir.";
         }
-        if (mod4Kontrol == false)// string içerisine attýðý sayý yok ise gireceði kontrol bloðu
+        if (mod4Kontrol == false)// string icerisine attigi sayi yok ise girecegi kontrol blogu
         {
-            mod4ListeString += "Dörde tam bölünen sayi bulunmamaktadýr.";
+            mod4ListeString += "Dorde tam bolunen sayi bulunmamaktadir.";
         }
-        if (mod5Kontrol == false)// string içerisine attýðý sayý yok ise gireceði kontrol bloðu
+        if (mod5Kontrol == false)// string icerisine attigi sayi yok ise girecegi kontrol blogu
         {
-            mod5ListeString += "Beþe tam bölünen sayi bulunmamaktadýr.";
+            mod5ListeString += "Bese tam bolunen sayi bulunmamaktadir.";
         }
-        Debug.Log("Tüm liste : " + listeString);
-        Debug.Log("Ýkiye bölünenler : " + mod2ListeString);
-        Debug.Log("Üçe bölünenler : " + mod3ListeString);
-        Debug.Log("Dörde bölünenler : " + mod4ListeString);
-        Debug.Log("Beþe bölünenler : " + mod5ListeString);
+        Debug.Log("Tum liste : " + listeString);
+        Debug.Log("Ikiye bolunenler : " + mod2ListeString);
+        Debug.Log("Uce bolunenler : " + mod3ListeString);
+        Debug.Log("Dorde bolunenler : " + mod4ListeString);
+        Debug.Log("Bese bolunenler : " + mod5ListeString);
     }
     // Start is called before the first frame update
     void Start()
